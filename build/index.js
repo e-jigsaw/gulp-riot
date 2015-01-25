02ec008879f03def027120de0c1784bfc90adb3a
jigsaw <jigsaw@live.jp>
2015-01-25 15:17:18 +0900

    add build file

---
+++ build/index.js
@@ -0,0 +1,26 @@
+(function() {
+  var compiler, gutil, through;
+
+  gutil = require('gulp-util');
+
+  through = require('through2');
+
+  compiler = require('../node_modules/riot/compiler');
+
+  module.exports = function() {
+    var transform;
+    transform = function(file, encoding, callback) {
+      if (file.isNull()) {
+        return callback(null, file);
+      }
+      if (file.isStream()) {
+        return callback(new gutil.PluginError('gulp-article', 'Stream not supported'));
+      }
+      file.contents = new Buffer(compiler(file.contents.toString()));
+      file.path = file.path.replace(/\.tag/, '.js');
+      return callback(null, file);
+    };
+    return through.obj(transform);
+  };
+
+}).call(this);
